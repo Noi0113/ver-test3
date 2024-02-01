d3d573c2775322b68fa2ebee622bc76e3046db2d
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 08:15:46 +0900

    Update DBtestsss.py

--- DBtestsss.py
+++ DBtestsss.py
@@ -4,7 +4,7 @@ import os
 import streamlit as st
 
 #os.chdir('ver-test3')
-st.title('DBテストsubprocessに賭けた版!！')
+st.title('DBテストsubprocessに賭けた版!')
 # ユーザーからの入力を収集
 user_input = st.text_input("何か入力してください")
 
@@ -24,24 +24,6 @@ if st.button("送信"):
 
     conn.close()
     st.write(f"あなたが入力したテキスト: {user_input}")
-
-    # Gitコマンドを実行
-    #try:
-    #    subprocess.check_call(['git', 'config', '--global', 'user.email', 's2110524@u.tsukuba.ac.jp'])
-    #    subprocess.check_call(['git', 'config', '--global', 'user.name', 'KNo0113'])
-     #   subprocess.check_call(['git', 'add', '--all'])
-     #   subprocess.check_call(['git', 'commit', '-m', 'Update database'])
-     #   subprocess.check_call(['git', 'reset', '--hard', 'origin/main'])
-      #  subprocess.check_call(['git', 'add', 'test-monketsu3.db'])
-      #  subprocess.check_call(['git', 'fetch', 'origin'])
-       # subprocess.check_call(['git', 'commit', '-m', 'Add SQLite database'])
-        #subprocess.check_call(['git', 'push'])
-        #st.write("データベースがGitHubリポジトリにプッシュされました。")
-    #except subprocess.CalledProcessError as e:
-     #   st.write("エラーが発生しました：", e)
-
-
-    #クローン作製
 
     try:
         # Gitのユーザー情報を設定
