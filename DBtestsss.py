84bd11127fb0deb27d7874165248b2e261983c56
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 01:11:14 +0900

    Create DBtestsss.py

---
+++ DBtestsss.py
@@ -0,0 +1,26 @@
+import sqlite3
+import subprocess
+import streamlit as st
+
+st.title('DBテスト！3')
+# ユーザーからの入力を収集
+user_input = st.text_input("何か入力してください")
+
+if st.button("送信"):
+    # データをデータベースに保存
+    conn = sqlite3.connect('test-monketsu3.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS user_inputs (input TEXT)")
+    c.execute("INSERT INTO user_inputs VALUES (?)", (user_input,))
+    conn.commit()
+    conn.close()
+    st.write(f"あなたが入力したテキスト: {user_input}")
+
+    # Gitコマンドを実行
+    try:
+        subprocess.check_call(['git', 'add', 'test-monketsu3.db'])
+        subprocess.check_call(['git', 'commit', '-m', 'Update database'])
+        subprocess.check_call(['git', 'push'])
+        st.write("データベースがGitHubリポジトリにプッシュされました。")
+    except subprocess.CalledProcessError as e:
+        st.write("エラーが発生しました：", e)
