9bd229b7e2f7a3b0eb573c863487b2f5c11301d6
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 06:37:45 +0900

    Update DBtestsss.py

--- DBtestsss.py
+++ DBtestsss.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 import streamlit as st
 
-st.title('DBテストsubprocessに賭けた版')
+st.title('DBテストsubprocessに賭けた版！！')
 # ユーザーからの入力を収集
 user_input = st.text_input("何か入力してください")
 
@@ -26,10 +26,10 @@ if st.button("送信"):
 
     # Gitコマンドを実行
     try:
-        subprocess.check_call(['git', 'add', '--all'])
-        subprocess.check_call(['git', 'commit', '-m', 'Update database'])
         subprocess.check_call(['git', 'config', '--global', 'user.email', 's2110524@u.tsukuba.ac.jp'])
         subprocess.check_call(['git', 'config', '--global', 'user.name', 'KNo0113'])
+        subprocess.check_call(['git', 'add', '--all'])
+        subprocess.check_call(['git', 'commit', '-m', 'Update database'])
         subprocess.check_call(['git', 'push'])
         st.write("データベースがGitHubリポジトリにプッシュされました。")
     except subprocess.CalledProcessError as e:
