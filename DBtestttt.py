1f98e5104069ad875442ca9f0fa679d2282c54c1
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 03:29:27 +0900

    Update DBtestttt.py

--- DBtestttt.py
+++ DBtestttt.py
@@ -12,13 +12,13 @@ user_input = st.text_input("Please enter your data")
 
 # ユーザーが何かを入力した場合
 if user_input:
-    c.execute("SELECT * FROM TestTable")
+    c.execute("SELECT * FROM TestTable3")
     rows = c.fetchall()
     for row in rows:
         st.write(row)
 
     # SQLクエリを作成（ここではテーブル名を 'your_table' と仮定）
-    query = f"INSERT INTO TestTable (univ) VALUES ('{user_input}')"
+    query = f"INSERT INTO TestTable3 (univ) VALUES ('{user_input}')"
 
     # クエリを実行
     c.execute(query)
